5e3cd04a16812af01bf028bff52af5cf5bab125b
Osameh Al-Kofahi <oalkofahi@gmail.com>
2023-06-25 10:30:10 -0500

    Fixed linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator Functions
 """
 
+
 def add(x, y):
     """ Add x and y and returns result """
     return x + y
+
 
 def subtract(x, y):
     """ Subtract x from y and returns result """
